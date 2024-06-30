63feafe0c6a3fe3e609c3e893b76c075e4e3b809
Andreas Erlandsson <andreaserlandsson@Andreas-MacBook-Pro.local>
2024-07-01 00:41:22 +0200

    Update install.sh

--- terraform/install.sh
+++ terraform/install.sh
@@ -1,13 +1,26 @@
 # Inspiration: https://ruan.dev/blog/2024/04/07/using-terraform-to-deploy-kind-kubernetes-clusters
 
-# Initialize Terraform
+# Deploy Kind cluster with Cilium and ArgoCD
 terraform init
-
-# View plan
-terraform plan
-
-# Deploy Kind cluster
 terraform apply -auto-approve
+
+# Deploy ingress
+cd applications/ingress
+terraform init
+terraform apply -auto-approve
+cd -
+
+# Deploy osiks-public ArgoCD project and nginx ArgoCD application
+cd argoapps/osiks
+terraform init
+terraform apply -auto-approve
+cd -
+
+# Deploy Grafana ArgoCD application
+cd argoapps/osiks-grafana
+terraform init
+terraform apply -auto-approve
+cd -
 
 # Set kube config file
 export KUBECONFIG=${PWD}/.kube/config
