573214f6683bc02f44dd204c4591915954b29207
Andreas Erlandsson <andreas.erlandsson@sr.se>
2024-07-01 09:30:20 +0200

    Don't use k alias for kubectl

--- terraform/install.sh
+++ terraform/install.sh
@@ -26,4 +26,4 @@ cd -
 export KUBECONFIG=${PWD}/.kube/config
 
 # Get ArgoCD initial admin password
-k -n argocd get secrets argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+kubectl -n argocd get secrets argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
