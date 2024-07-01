9c83ee3a69ff0dcb43ba63154a1e11765fd04dbc
Andreas Erlandsson <andreas.erlandsson@sr.se>
2024-07-01 08:48:30 +0200

    Add prerequisites installation script

---
+++ terraform/install_prerequisites.sh
@@ -0,0 +1,12 @@
+# Install Podman Desktop
+brew install podman-desktop
+
+# Install Terraform
+brew tap hashicorp/tap
+brew install hashicorp/tap/terraform
+
+# Install Helm
+brew install helm
+
+# Install kubectl
+brew install kubectl
