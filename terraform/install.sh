a950a48a452f7e94c83ccbf1969c8bf95d88a63a
Andreas Erlandsson <andreas.erlandsson@sr.se>
2024-07-01 09:05:07 +0200

    Extract initial admin password

--- terraform/install.sh
+++ terraform/install.sh
@@ -24,3 +24,6 @@ cd -
 
 # Set kube config file
 export KUBECONFIG=${PWD}/.kube/config
+
+# Get ArgoCD initial admin password
+k -n argocd get secrets argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
